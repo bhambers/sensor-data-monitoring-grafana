664a903dca84040ce634d81736c4bf7fcc5ab69a
bhambers <bhambers@rpi-01>
2026-01-09 13:55:09 +0000

    Capture sonar distance

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import adafruit_dht as dht_sensor
+import adafruit_hcsr04 as sonar_sensor
 import time
 import board
 from flask import Flask, Response
@@ -6,6 +7,16 @@ from prometheus_client import Counter, Gauge, start_http_server, generate_latest
 
 content_type = str('text/plain; version=0.0.4; charset=utf-8')
 dhtDevice = dht_sensor.DHT22(board.D4)
+sonar = sonar_sensor.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+
+def get_sonar_readings():
+    try:
+        distance = sonar.distance
+        distance = format(distance, ".3f")
+        response = {"distance": distance}
+        return response
+    except:
+        return {"distance": 0}
 
 def get_temperature_readings():
     temperature = dhtDevice.temperature
@@ -28,6 +39,12 @@ def get_temperature_readings():
 
 app = Flask(__name__)
 
+current_distance = Gauge(
+        'current_distance',
+        'the current distance in cm, tih is a gauge as the value can increase or decrease',
+        ['room']
+)
+
 current_humidity = Gauge(
         'current_humidity',
         'the current humidity percentage, this is a gauge as the value can increase or decrease',
@@ -43,9 +60,12 @@ current_temperature = Gauge(
 @app.route('/metrics')
 def metrics():
     metrics = get_temperature_readings()
+    distance = get_sonar_readings()
+    metrics = metrics | distance
     print(metrics)
     current_humidity.labels('study').set(metrics['humidity'])
     current_temperature.labels('study').set(metrics['temperature'])
+    current_distance.labels('study').set(metrics['distance'])
     return Response(generate_latest(), mimetype=content_type)
 
 if __name__ == '__main__':
